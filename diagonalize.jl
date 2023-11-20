cabb1049a4e4f0480f2b26f8443f4a281cca1d5b
djuliannader <djuliannader@gmail.com>
2023-11-20 09:18:47 -0500

    cambios survival probability

--- diagonalize.jl
+++ diagonalize.jl
@@ -3,7 +3,7 @@ push!(LOAD_PATH, pwd())
 using LinearAlgebra
 export diagonalization
 
-function diagonalization(cx::Vector{Float64},cp::Vector{Float64},Nmax::Int64,hbar::Float64)
+function diagonalization(cx::Vector{Float64},cp::Vector{Float64},Nmax::Int64,hbar::Float64,flag::Int64)
 	 dar=[0.0 for i in 1:Nmax]
 	 d=[0.0 for i in 1:Nmax+1]
 	 dab=[(i)^(1/2) for i in 1:Nmax]
@@ -12,13 +12,16 @@ function diagonalization(cx::Vector{Float64},cp::Vector{Float64},Nmax::Int64,hba
 	 xo=((hbar/2.)^(1/2))*(a + ad);
 	 po=-im*((hbar/2.)^(1/2))*(a - ad)
 	 H=sum(cx[i]*xo^(i) for i in 1:length(cx))+sum(cp[i]*po^(i) for i in 1:length(cp))
-	 return eigvals(H)
+	 if flag==1
+	    energies=eigvals(H)
+	    return energies
+	 end
+	 if flag==2
+	    states=eigvecs(H)
+	    return states
+	 end
 	 end
 	 
-#L1=[0,-10.0,0,1]
-#L2=[0,0.5]
-#ex=diagonalization(L1,L2,100,0.5)
-#println(length(ex))
 
 
 
--- dynamics.jl
+++ dynamics.jl
@@ -16,31 +16,37 @@ function initialcoherent(xi::Float64,pi::Float64,hbar::Float64,Nmax::Int64)
 	 return csf
 	 end
 
-#function survivalp(cx::Vector{Float64},cp::Vector{Float64},psi0::Vector{Complex{Float64}},tmax::Float64)
-#         dar=[0.0 for i in 1:Nmax]
-#	 d=[0.0 for i in 1:Nmax+1]
-#	 dab=[(i)^(1/2) for i in 1:Nmax]
-#	 a=Array(Tridiagonal(dar,d,dab))
-#	 ad=a'
-#	 xo=((10000/2.)^(1/2))*(a + ad);
-#	 po=-im*((10000/2.)^(1/2))*(a - ad)
-#	 H=sum(cx[i]*xo^(i) for i in 1:length(cx))+sum(cp[i]*po^(i) for i in 1:length(cp))
-#	 tint=tmax/0.1
-#	 nt=parse(Int64,tmax/0.1)
-#	 t=0.0
-#	 psi0a=Array{Complex{Float64}}(undef,1,Nmax+1) 
-#	 for k in 1:length(psi0)
-#	     psi0a[1,k]=conj(psi0[k])
-#        end
-#	 open("survivalprobability.dat","w") do io
-#	 for i in 1:nt+1
-#	     evol=exp(-im*H*t/10000)
-#	     sp=
-#	     sp=abs2(sp)
-#	     println(io,round(t,digits=16)," ",round(sp,digits=16))
-#	     t=t+tint
-#	     end
-#	  end
+function survivalp(cx::Vector{Float64},cp::Vector{Float64},psi0::Vector{Complex{Float64}},tmax::Float64,hbar::Float64,Nmax::Int64)
+         dar=[0.0 for i in 1:Nmax]
+	 d=[0.0 for i in 1:Nmax+1]
+	 dab=[(i)^(1/2) for i in 1:Nmax]
+	 a=Array(Tridiagonal(dar,d,dab))
+	 ad=a'
+	 xo=((hbar/2.)^(1/2))*(a + ad);
+	 po=-im*((hbar/2.)^(1/2))*(a - ad)
+	 H=sum(cx[i]*xo^(i) for i in 1:length(cx))+sum(cp[i]*po^(i) for i in 1:length(cp))
+	 tint=0.05
+	 nt=trunc(Int,tmax/tint)
+	 t=0.0
+	 psi0a=Array{Complex{Float64}}(undef,1,Nmax+1) 
+	 for k in 1:length(psi0)
+	     psi0a[1,k]=conj(psi0[k])
+         end
+	 open("survivalprobability.dat","w") do io
+ 	 for i in 1:nt+1
+ 	     evol=exp(-im*H*t/hbar)
+	     psi0t=evol*psi0
+ 	     sp=psi0a*psi0t
+ 	     sp=abs2(sp[1])
+ 	     println(io,t," ",round(sp,digits=16))
+ 	     t=t+tint
+ 	     end
+ 	 end
+	 println("-------------   Go to file survivalprobability.dat to see the survival probability  ----------------")
+	 println("             The file contains SP from 0 to ",tmax," in steps of ",tint," time units               ")
+	 println("--------------------------------------------------------------------------------------------------- ")
+	 return "Done"
+	 end	 
 
 function timeevolution(cx::Vector{Float64},xlim::Vector{Float64},cp::Vector{Float64},plim::Vector{Float64},psi0::Vector{Complex{Float64}},Tl::Vector{Float64},hbar::Float64,Nmax::Int64)
 	 dar=[0.0 for i in 1:Nmax]
@@ -48,8 +54,8 @@ function timeevolution(cx::Vector{Float64},xlim::Vector{Float64},cp::Vector{Floa
 	 dab=[(i)^(1/2) for i in 1:Nmax]
 	 a=Array(Tridiagonal(dar,d,dab))
 	 ad=a'
-	 xo=((10000/2.)^(1/2))*(a + ad);
-	 po=-im*((10000/2.)^(1/2))*(a - ad)
+	 xo=((hbar/2.)^(1/2))*(a + ad);
+	 po=-im*((hbar/2.)^(1/2))*(a - ad)
 	 H=sum(cx[i]*xo^(i) for i in 1:length(cx))+sum(cp[i]*po^(i) for i in 1:length(cp))
 	 pmax=plim[2]
 	 pmin=plim[1]
@@ -85,11 +91,11 @@ function timeevolution(cx::Vector{Float64},xlim::Vector{Float64},cp::Vector{Floa
 	   end
 	 end
 	 end
-	 println("-------------Go to file tevolution.dat for dynamics----------------")
-	 println("The file contains ",length(Tl)," screenshots of the dynamics    ")
-	 println("Each screenshot corresponds to 2601 rows of the data file          ")
-	 println("-------------------------------------------------------------------")
-	 return "done"
+	 println("------------------- Go to file tevolution.dat to see screenshots of the dynamics  ----------------")
+	 println("                     The file contains ",length(Tl)," screenshots of the dynamics                                  ")
+	 println("                       Each screenshot corresponds to 2601 rows of the data file                                   ")
+	 println("--------------------------------------------------------------------------------------------------")
+	 return "Done"
 	 end
 
 
